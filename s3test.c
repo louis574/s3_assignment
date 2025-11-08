2ef90a50e64f907bfec08e05dbfacb49122f735b
louis574 <loujamcan@btinternet.com>
2025-11-08 15:24:41 +0000

    working </> detection

---
+++ s3test.c
@@ -0,0 +1,30 @@
+#include "s3.h"
+
+int main(int argc, char *argv[]){
+
+    ///Stores the command line input
+    char line[MAX_LINE];
+
+    ///Stores pointers to command arguments.
+    ///The first element of the array is the command name.
+    char *args[MAX_ARGS];
+
+    ///Stores the number of arguments
+    int argsc;
+
+    while (1) {
+        read_command_line(line);
+
+        if(redirection_detect(line)){
+            fprintf(stdout, "Detected\n");
+        }
+
+        else{
+
+            fprintf(stdout, "Not Detected\n");
+        }
+    }
+
+    return 0;
+    
+}
